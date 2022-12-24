28dcaf9b13a7a44600ac0156fd49d04fde5e796b
aleksandrdiptan <diptan@huntflow.ru>
2022-12-24 14:00:12 +0300

    Добавлено:  - обработка вебхуков по вакансии.

--- api_methods.py
+++ api_methods.py
@@ -6,6 +6,28 @@ import requests
 from dotenv import load_dotenv
 
 
+def add_applicant_to_vacancy(api_url: str, org_id: str, applicant_id: str, vacancy_id: int, vacancy_status_id: str,
+                             headers: dict) -> dict:
+    payload = {"vacancy": vacancy_id,
+               "status": vacancy_status_id,
+               }
+    response = requests.post(f'{api_url}/v2/accounts/{org_id}/applicants/{applicant_id}/vacancy', headers=headers,
+                             json=payload)
+    response.raise_for_status()
+
+    return response.json()
+
+
+def search_applicants_with_position(api_url: str, org_id: str, applicant_position_name: str, headers: dict) -> dict:
+    params = {"q": applicant_position_name,
+              "field": "position",
+              }
+    response = requests.get(f'{api_url}/v2/accounts/{org_id}/applicants/search', headers=headers, params=params)
+    response.raise_for_status()
+
+    return response.json()
+
+
 def update_applicant_tags(api_url: str, org_id: str, applicant_id: int, applicant_tags: list, headers: dict) -> int:
     payload = {"tags": applicant_tags}
     response = requests.post(f'{api_url}/v2/accounts/{org_id}/applicants/{applicant_id}/tags', headers=headers,
@@ -88,11 +110,19 @@ def main():
     hf_token = os.environ['HF_API_TOKEN']
     hf_api_url = os.environ['HF_API_URL']
     org_id = os.environ['ORG_ID']
+    vacancy_position = os.environ['VACANCY_POSITION']
+    start_vacancy_status_id = os.environ['START_VACANCY_STATUS_ID']
     headers = {'Authorization': f'Bearer {hf_token}'}
 
     accounts_by_token = get_accounts(hf_api_url, headers)
     account_vacancy_statuses = get_account_vacancies_statuses(hf_api_url, org_id, headers)
     hired_status_id = get_hired_vacancy_status_id(account_vacancy_statuses)
+    # ---------------------------------------------------------------------------------------------------------------
+    applicants_with_requested_position = search_applicants_with_position(hf_api_url, org_id, vacancy_position,
+                                                                         headers)
+    if applicants_with_requested_position['total_items'] > 0:
+        applicant_id = applicants_with_requested_position['items'][0]['id']
+        pprint(add_applicant_to_vacancy(hf_api_url, org_id, applicant_id, 163, start_vacancy_status_id, headers))
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from pprint import pprint
 
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request
@@ -14,10 +15,44 @@ if Path.exists(dot_env_path):
 hf_token = os.environ['HF_API_TOKEN']
 hf_api_url = os.environ['HF_API_URL']
 account_id = os.environ['ORG_ID']
+start_vacancy_status_id = os.environ['START_VACANCY_STATUS_ID']
 hired_account_status_id = int(os.environ['HIRED_STATUS_ID'])
+vacancy_position = os.environ['VACANCY_POSITION']
 tag_name = os.environ['TAG_NAME']
 tag_color = os.environ['TAG_COLOR']
 headers = {'Authorization': f'Bearer {hf_token}'}
+
+
+@app.post("/vacancy")
+async def applicant(request: Request):
+    event = request.headers.get("X-Huntflow-Event")
+
+    if event != "VACANCY":
+        return {"status": "ERR: event is not VACANCY"}
+
+    hf_body = await request.json()
+    vacancy_state = hf_body["event"]["vacancy_log"]["state"]
+
+    if vacancy_state != "CREATED":
+        return {"status": "ERR: Vacancy state is not CREATED"}
+
+    vacancy_id = hf_body["event"]["vacancy"]["id"]
+    created_vacancy_position = hf_body["event"]["vacancy"]["position"]
+    applicants_with_requested_position = api_methods.search_applicants_with_position(hf_api_url, account_id,
+                                                                                     vacancy_position,
+                                                                                     headers)
+    if applicants_with_requested_position['total_items'] == 0:
+        return {"status": "ERR: There is no applicant without vacancy or needed position name"}
+
+    applicant_id = applicants_with_requested_position['items'][0]['id']
+
+    if created_vacancy_position != vacancy_position:
+        return {"status": "ERR: Vacancy position is not equal applicant position"}
+
+    api_methods.add_applicant_to_vacancy(hf_api_url, account_id, applicant_id, vacancy_id,
+                                         start_vacancy_status_id, headers)
+
+    return {"status": "OK"}
 
 
 @app.post("/applicant")
@@ -25,7 +60,7 @@ async def applicant(request: Request):
     event = request.headers.get("X-Huntflow-Event")
 
     if event != "APPLICANT":
-        return {"status": "ERR"}
+        return {"status": "ERR: event is not APPLICANT"}
 
     hf_body = await request.json()
     hf_applicant_status = hf_body["event"]["applicant_log"]["status"]
@@ -36,7 +71,7 @@ async def applicant(request: Request):
     applicant_id = int(hf_body["event"]["applicant"]["id"])
 
     if hf_applicant_status != hired_account_status_id or not hf_applicant_status:
-        return {"status": "EXIT"}
+        return {"status": "ERR: Status not a hired"}
 
     applicant_tags = api_methods.get_applicant_tags(hf_api_url, account_id, applicant_id, headers)["tags"]
     account_tags = api_methods.get_account_tags(hf_api_url, account_id, headers)
